d87da6b5df574cf459f06392412a0049d7331b59
gitket <ketwowplayer@gmail.com>
2020-06-06 00:59:51 -0600

    Update trevorproj.js

--- TrevorProject/trevorproj.js
+++ TrevorProject/trevorproj.js
@@ -7,20 +7,23 @@ let eventsLimit = 1,
     fadeoutTime = 999;
 	startingAmount = 0;
 	first = true;
-let timer;
+let intervalTimer;
 let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 let targetUrl = 'https://give.thetrevorproject.org/frs-api/fundraising-pages/';
+let lastDisplayedId = '';
+
 
 window.addEventListener('onWidgetLoad', function (obj) {
   
   const fieldData = obj.detail.fieldData;
   audio = fieldData.changeSound;
-  cID = fieldData.campaignID;
+  scare = fieldData.scareSound;
+  cID = fieldData.campaignID; 
   
   if(cID){
     getInitial();
     getDonors();
-    timer = setInterval(()=>{
+    intervalTimer = setInterval(()=>{
       fetch(proxyUrl + targetUrl + cID)
       .then(blob => blob.json())
       .then(data => {
@@ -43,19 +46,32 @@ window.addEventListener('onWidgetLoad', function (obj) {
 
 
 function getDonors(){
-  let donorFeed = '/initial-feed?with=member,linkable&sort=created_at:desc&per_page=6';
+  let donorFeed = '/feed-items?with=member,linkable&sort=created_at:desc&per_page=5&campaignId=24399&page=1';
     fetch(proxyUrl + targetUrl + cID + donorFeed)
       .then(blob => blob.json())
       .then(data => {
+       console.log(data)
+		let latestDonorId = data.data[0].id;
       	let donorhtmlstr = '';
       	data.data.forEach(element =>{
           if(element.linkable){
           	let details = element.linkable;
-            let donorName = details.member_name;
+            let donorName ='Anonymous';
+            if(details.member_name){
+              let anonName = details.member_name.split(" ");
+              donorName = anonName[0] + ' ' + anonName[1].charAt(0);
+            }
             let donorAmt = details.donation_gross_amount;
             donorhtmlstr += `<span class='donor-spacer'>${donorName} - $${donorAmt}</span>`
           }
-        })
+        });
+      if(first){
+      	lastDisplayedId = latestDonorId;
+      }else{ 
+        console.log('why u not first')
+		setTimeout(()=>queueDonations(data.data), 0);
+
+      }
       
       $(".marquee").remove();
       let donors =   `<p class="marquee">`+donorhtmlstr +`
@@ -83,6 +99,69 @@ function getInitial(){
       .catch(e => {
         addEvent('Error', e.toString());
       });
+}
+
+// Returns a Promise that resolves after "ms" Milliseconds
+function timer(ms) {
+ return new Promise(res => setTimeout(res, ms));
+}
+
+async function queueDonations (data) { // We need to wrap the loop into an async function for this to work
+  for (var i = 0; i < data.length; i++) {
+    if(data[i].id != lastDisplayedId){
+      	let tmpName = data[i].member_name.split(" ");
+        let anonyName = tmpName[0] + ' ' + tmpName[1].charAt(0);
+       donationAlert(anonyName, data[i].linkable.donation_gross_amount, data[i].linkable.comment );
+       await timer(10000); // then the created Promise can be awaited
+  	}else{
+       break;
+    }    
+  }
+  lastDisplayedId = data[0].id;
+}
+
+function donationAlert(name, donoAmt, message){
+
+  let alertDialog;
+  if(donoAmt >= 50){
+  alertDialog= `
+	<div class='alert'>
+		<div>
+    		<span class='donor-highlight'>${name}</span> 
+			donated <span class='donor-highlight'>$${donoAmt}</span> 
+			to The Trevor Project
+  		</div>
+  		<br>
+  		<div class='donor-message'>
+    		${message}
+ 	 	</div>
+      <audio id="audio" autoplay >
+      	<source id="alertsound" src="${scare}" type="audio/ogg">
+      </audio>
+	</div>
+	`;
+  } else{
+      alertDialog= `
+	<div class='alert'>
+		<div>
+    		<span class='donor-highlight'>${name}</span> 
+			donated <span class='donor-highlight'>$${donoAmt}</span> 
+			to The Trevor Project
+  		</div>
+  		<br>
+  		<div class='donor-message'>
+    		${message}
+ 	 	</div>
+      <audio id="audio" autoplay >
+      	<source id="alertsound" src="${audio}" type="audio/ogg">
+      </audio>
+	</div>
+	`;
+  }
+  $(".alert-container").prepend(alertDialog);
+  setTimeout(()=>{
+    $(".alert").remove();
+  }, 7000);
 }
 
 function addEvent(total, goal) {
